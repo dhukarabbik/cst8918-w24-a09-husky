14860db533253db0d95af11c76dd0bd09e59ec92
dhukarabbik <dhuk0001@algonquinlive.com>
2024-03-08 17:16:06 -0500

    Corrected formatting diff

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ terraform {
       version = "3.58.0"
     }
   }
+
+  required_version = ">= 0.12.0"  # Specify the minimum Terraform version needed for your configuration
 }
 
 provider "azurerm" {
